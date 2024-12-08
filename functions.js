5b6094cc859a3baa88a3d1f86c4473b5ad492879
Liutauras <liutauraskuzma@gmail.com>
2024-12-08 17:19:39 +0200

    fineshed

--- functions.js
+++ functions.js
@@ -43,16 +43,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function updateClock() {
   const now = new Date();  
-  const hours = String(now.getHours()).padStart(2, '0');  
-  const minutes = String(now.getMinutes()).padStart(2, '0');  
-  const seconds = String(now.getSeconds()).padStart(2, '0');  
+  const hours = String(now.getHours());  
+  const minutes = String(now.getMinutes());  
+  const seconds = String(now.getSeconds());  
  
   const formattedTime = `${hours}:${minutes}:${seconds}`;
  
   document.getElementById('clock').textContent = formattedTime;
 }
 
-// Atnaujinti laikrodį kas sekundę
+ 
 setInterval(updateClock, 1000);
-// Iš karto atnaujinti laiką, kad nedingtų pradinis vėlavimas
+ 
 updateClock();
